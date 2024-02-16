dc123fda9224a8a6b5201152e5756eaa5566bc9c
jthapata <jessezap01@gmail.com>
2024-02-15 21:44:47 -0700

    adding delete button

--- add-item.js
+++ add-item.js
@@ -3,18 +3,22 @@ const newItemName = document.getElementById('new-item-name')
 const newItemButton = document.getElementById('new-item-button')
 const cancelNewItem = document.getElementById('cancel-new-item-button')
 const itemDisplay = document.getElementById('display-add-item')
-const taskList = document.getElementById('taskList')
+const taskList = document.getElementById('itemList')
 
 function addItem (name) {
     let newDiv = document.createElement('div')
     let newInput = document.createElement('input')
     let newLabel = document.createElement('label')
+    let newDelete = document.createElement('i')
+    newDelete.id = 'deleteItem'
+    newDelete.setAttribute('class', 'fa-solid fa-trash-can ml-3')
     newLabel.setAttribute('for', name)
     newLabel.innerHTML = name
     newInput.type = 'checkbox'
     newInput.name = name
     newDiv.append(newInput)
     newDiv.append(newLabel)
+    newDiv.append(newDelete)
     newDiv.className = 'item'
     taskList.append(newDiv)
 }
