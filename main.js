75b157735687de042b809f8d1c26193f2f8f3f32
jthapata <jessezap01@gmail.com>
2024-02-20 07:24:04 -0700

    js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+
+function createNewElement (name) {
+    let newDiv = document.createElement('div')
+    let newInput = document.createElement('input')
+    let newLabel = document.createElement('label')
+    let newDelete = document.createElement('i')
+    newDelete.id = 'deleteItem'
+    newDelete.setAttribute('class', 'fa-solid fa-trash-can ml-3')
+    newLabel.setAttribute('for', name)
+    newLabel.innerHTML = name
+    newInput.type = 'checkbox'
+    newInput.name = name
+    newDiv.append(newInput)
+    newDiv.append(newLabel)
+    newDiv.append(newDelete)
+    newDiv.className = 'item'
+    newDelete.addEventListener('click', function() {
+        newDiv.remove()
+    })
+    return(newDiv)
+}
+
+console.log(createNewElement('pizza'))
 console.log(localStorage)
