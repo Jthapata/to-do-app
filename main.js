fd631afb67f2a07e5289496e4ad2a7d24d36afe0
jthapata <jessezap01@gmail.com>
2024-02-21 07:35:07 -0700

    adding js notes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // bonus: animate creating and deleting task/list
 // bug: when there is a pop up, make the background unclickable, that way you can't bring up multiple pop ups
 // bug: when a list is deleted it removes the items from display
+// code change: move css from css file to html file for create list button
 
 const sidebar = document.getElementById('Lists')
 const taskList = document.getElementById('itemList')
